8649b7aa1f6abf6294eda6fa9b2e158b8ec4bc9e
SCOTTY0101 <alan777555@hotmail.com>
2023-04-23 08:22:33 +1000

    boolean and if question variable

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -5,11 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo</title>
+    <style>
+    h1 {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
 </head>
+
 <body>
+    
     <?php 
-    print "working?";
+    $book = "Hell";
+    $read = true;
+   // echo "$greeting Awaits you";
+   // echo "<p>ghgh</p>";
+   if ($read == true) {
+    $message = " boolean You read the book $book ";
+   }
     
     ?>
+   
+
+    <h1>
+        You read the book <?php echo $book; ?>.
+        <?php echo $message; ?>
+    </h1>
+
 </body>
 </html>
