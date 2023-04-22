1994f11409404b5c286fa614cc5bff0e7f22fdcf
SCOTTY0101 <alan777555@hotmail.com>
2023-04-23 06:20:39 +1000

    using wrong dir. demo php -S WORKING

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -4,26 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>The Demo</title>
+    <title>Demo</title>
 </head>
 <body>
-    <h1>Hell world the second</h1>
-    <?php
-    echo "<h1>Hello world</h1>";
-      /*  echo "is this php working";
-        echo "nect line to git save point";
-
-
-        $x = 5;
-        $y = 10;
-
-        function myTest() {
-            $GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
-        } 
-
-        myTest();
-        echo $y;
-        */
-    ?> 
+    <?php 
+    print "working?";
+    
+    ?>
 </body>
 </html>
