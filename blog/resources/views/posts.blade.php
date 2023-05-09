733cac22f607e8ebd0ea967b2a2b84decfb4a876
SCOTTY0101 <alan777555@hotmail.com>
2023-05-09 17:03:43 +1000

    html link pages on blog

---
+++ blog/resources/views/posts.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My Blog</title>
+
+    <link rel="stylesheet" href="/app.css">
+   <!-- <script src="/app.js"></script> -->
+</head>
+<body>
+    <article>
+        <h1><a href="/post/my-first-post">My First Blog</a></h1>
+        <p>
+            Lorem  POSTS PHP ipsum dolor sit amet consectetur adipisicing elit. 
+            Provident magni rem commodi molestiae nobis hic minus! 
+            Vel odio tempore labore? Ipsam, laborum! Dolores atque odio, 
+            perspiciatis praesentium dolorum quam magnam.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post/my-second-post">My Second Blog</a></h1>
+        <p>
+            Lorem POSTS PHP ipsum dolor sit amet consectetur adipisicing elit. 
+            Provident magni rem commodi molestiae nobis hic minus! 
+            Vel odio tempore labore? Ipsam, laborum! Dolores atque odio, 
+            perspiciatis praesentium dolorum quam magnam.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post/my-third-post">My Third Blog</a></h1>
+        <p>
+            Lorem POSTS PHP ipsum dolor sit amet consectetur adipisicing elit. 
+            Provident magni rem commodi molestiae nobis hic minus! 
+            Vel odio tempore labore? Ipsam, laborum! Dolores atque odio, 
+            perspiciatis praesentium dolorum quam magnam.
+        </p>
+    </article>
+    
+    
+    
+</body>
+</html>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,7 +14,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
+    
 });
 
+Route::get('post/{post}', function($slug) {
+    //return  $slug;
+    //$path = (__DIR__ . "/../resources/posts/{$slug}.html");
+
+    //ddd($path);
+    
+    if (! file_exists($path = (__DIR__ . "/../resources/posts/{$slug}.html"))) {
+        return redirect('/');
+        //return ddd('file does not exist.');
+        //abort(404);
+    };
+
+    $post = cache()->remember("posts.{$slug}", 5, fn() => file_get_contents($path));
+       // var_dump('file_get_contents');
+
+    
+   # $post = file_get_contents($path);
+    
+    return view('post', [ 'post' => $post ]); 
+})->where('post', '[A-z_\-]+');     //->whereAlpha('post');  Method, command +click            //->where('post', '[A-z_-\]+');
 //Route::get('register', [RegisterController::class, 'create']);
