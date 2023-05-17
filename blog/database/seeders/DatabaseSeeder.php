b2954a172ceee44cee0a84f948a85bf57af1510f
SCOTTY0101 <alan777555@hotmail.com>
2023-05-17 20:01:13 +1000

    Blog page updated and working

--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 
 use \App\Models\User;
 use \App\Models\Category;
+use \App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,15 +16,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+
          $user = User::factory()->create();
 
-        Category::create([
+        $personal = Category::create([
             'name' => 'Personal',
-            'slug' => 'persona;'
+            'slug' => 'personal'
         ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-first-post',
+            'excerpt' => 'More about the first post inside.',
+            'body' => 'FIRST BODY BODT INFO jdjdjdjdjdjdjdjdjdjdjjjjd END END END'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => 'More about the WORK post inside.',
+            'body' => 'WORK WORK WORK WORK WORK na na na. BODT INFO jdjdjdjdjdjdjdjdjdjdjjjjd END END END'
+        ]);
+
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
-        // ]);
+        // ]);*/
     }
 }
