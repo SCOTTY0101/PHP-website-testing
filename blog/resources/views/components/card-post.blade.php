3f6cf31aa5ff89db68bc7d6183652e6cb36256c4
SCOTTY0101 <alan777555@hotmail.com>
2023-05-22 21:26:25 +1000

    change column grid layout. db 5 enteries

--- blog/resources/views/components/card-post.blade.php
+++ blog/resources/views/components/card-post.blade.php
@@ -8,9 +8,9 @@
         <div class="mt-8 flex flex-col justify-between">
             <header>
                 <div class="space-x-2">
-                    <a href="/category/{{ $post-category->slug }}"
+                    <a href="/categories/{{ $post->category->slug }}"
                         class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                        style="font-size: 10px">{{ $post->category-name }}</a>
+                        style="font-size: 10px">{{ $post->category->name }}</a>
 
                     <a href="#"
                         class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
@@ -49,8 +49,7 @@
 
                 <div>
                     <a href="#"
-                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
-                    >
+                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">
                         Read More
                     </a>
                 </div>
--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -3,14 +3,18 @@
 @include ('_post-header');
 
 <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-    <x-post-featured-card :post="$posts->first()"/>
-
-    <div class="lg:grid lg:grid-cols-2">
-        @foreach( $posts->skip(1) as $post)
-        <x-card-post :post="$posts"/>
-        @endforeach
-    </div>
-
+    @if ($posts->count())
+    <x-post-featured-card :post="$posts[1]"/>
+        @if ($posts->count() > 1)
+        <div class="lg:grid lg:grid-cols-6">
+            @foreach ($posts->skip(1) as $post)
+            <x-card-post :post="$post" />
+            @endforeach
+        @endif
+        </div>
+    @else
+    <p class="text-center">No posts yet. Please check later.</p>
+    @endif
 </main>
 </x-layout>
 
