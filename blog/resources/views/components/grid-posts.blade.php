41503ee3cfb2fd490c7cea61a6334c174457840e
SCOTTY0101 <alan777555@hotmail.com>
2023-05-23 21:00:50 +1000

    code reset point.

---
+++ blog/resources/views/components/grid-posts.blade.php
@@ -0,0 +1,17 @@
+@props(['posts'])
+
+
+<x-post-featured-card :post="$posts[0]"/>
+
+                    @if ($posts->count() > 1)
+                        <div class="lg:grid lg:grid-cols-6">
+                            @foreach ($posts->skip(1) as $post)
+                        
+                                <x-card-post 
+                                :post="$post" 
+                                class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2 bg-red-500'}}"
+                                />
+                            @endforeach
+                        </div>
+                    @endif
+                
--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -1,25 +1,31 @@
 
 <x-layout> 
-@include ('_post-header');
+   
+    @include ('_post-header');
 
-<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-    @if ($posts->count())
-    <x-post-featured-card :post="$posts[1]"/>
-        @if ($posts->count() > 1)
-        <div class="lg:grid lg:grid-cols-6">
-            @foreach ($posts->skip(1) as $post)
-          
-            <x-card-post 
-            :post="$post" 
-            class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2 bg-red-500' }}"
-            />
-            @endforeach
-        @endif
-        </div>
-    @else
-    <p class="text-center">No posts yet. Please check later.</p>
-    @endif
-</main>
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            @if ($posts->count())
+            <x-post-featured-card :post="$posts[0]"/>
+
+                @if ($posts->count() > 1)
+                <div class="lg:grid lg:grid-cols-6">
+                    @foreach ($posts->skip(1) as $post)
+                
+                        <x-card-post 
+                        :post="$post" 
+                        class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2 bg-red-500'}}"
+                        />
+                    @endforeach
+                </div>
+                @endif
+
+
+                
+            @else
+                <p class="text-center">No posts yet. Please check later.</p>
+            @endif
+        </main>
+  
 </x-layout>
 
 
