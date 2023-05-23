5086daf5b175f2c0e785c8baf38743350ad1fa7f
SCOTTY0101 <alan777555@hotmail.com>
2023-05-23 18:44:40 +1000

    blog tailwind update

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -8,7 +8,11 @@
         @if ($posts->count() > 1)
         <div class="lg:grid lg:grid-cols-6">
             @foreach ($posts->skip(1) as $post)
-            <x-card-post :post="$post" />
+          
+            <x-card-post 
+            :post="$post" 
+            class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2 bg-red-500' }}"
+            />
             @endforeach
         @endif
         </div>
