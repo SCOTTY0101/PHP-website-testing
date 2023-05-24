bcd46eb5a1a5dd76278cfc741578891dbaf6a69b
SCOTTY0101 <alan777555@hotmail.com>
2023-05-24 19:20:15 +1000

    props posts added post to props array. Now working

--- blog/resources/views/components/post-featured-card.blade.php
+++ blog/resources/views/components/post-featured-card.blade.php
@@ -11,7 +11,7 @@
                     <div class="space-x-2">
                         <a href="/categories/{{ $post->category->slug }}"
                            class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                           style="font-size: 10px">{{ $post->category->name }}Techniques</a>
+                           style="font-size: 10px"> {{ $post->category->name }} Techniques</a>
 
                         <a href="#"
                            class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
@@ -56,3 +56,4 @@
             </div>
         </div>
     </article>
+    
--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -1,24 +1,10 @@
 
 <x-layout> 
-   
-    @include ('_post-header');
+    @include ('_post-header')
 
         <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
             @if ($posts->count())
-            <x-post-featured-card :post="$posts[0]"/>
-
-                @if ($posts->count() > 1)
-                <div class="lg:grid lg:grid-cols-6">
-                    @foreach ($posts->skip(1) as $post)
-                
-                        <x-card-post 
-                        :post="$post" 
-                        class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2 bg-red-500'}}"
-                        />
-                    @endforeach
-                </div>
-                @endif
-
+            <x-grid-posts $posts="posts" />
 
                 
             @else
