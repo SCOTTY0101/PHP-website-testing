ebcf8e9630d10b3c1b9d56e0da62841ac2615988
SCOTTY0101 <alan777555@hotmail.com>
2023-05-24 19:50:41 +1000

    x-grid-card link now working

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -1,17 +1,17 @@
 
 <x-layout> 
-    @include ('_post-header')
+    @include ('_post-header');
 
         <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
             @if ($posts->count())
-            <x-grid-posts $posts="posts" />
+            <x-grid-posts :posts="$posts" />
+
 
                 
             @else
                 <p class="text-center">No posts yet. Please check later.</p>
             @endif
         </main>
-  
 </x-layout>
 
 
