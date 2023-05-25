6aeab0eecd110926662b0827edc2e0a1ababb374
SCOTTY0101 <alan777555@hotmail.com>
2023-05-25 18:35:10 +1000

    clean up code

--- blog/resources/views/post.blade.php
+++ blog/resources/views/post.blade.php
@@ -28,7 +28,7 @@
                         </h1>
 
                         <span class="mt-2 block text-gray-400 text-xs">
-                                Published <time>{{ $post->created_at->diffForHumans() }}...1 day ago</time>
+                                Published <time>{{ $post->created_at->diffForHumans() }}</time>
                             </span>
                     </div>
                 </header>
@@ -50,9 +50,9 @@
                     </div>
 
                     <div class="hidden lg:block">
-                        <a href="/$posts/{{ $post->slug }}"
+                        <a href="/posts/{{ $post->slug }}"
                            class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
-                        >Read More</a>
+                        >Read More{{ $post->title}}</a>
                     </div>
                 </footer>
             </div>
