2e24e6d852694f93e4703936711e6751d2affa5a
SCOTTY0101 <alan777555@hotmail.com>
2023-05-29 18:44:58 +1000

    activate search bar in header

--- blog/dump code /dump.php
+++ blog/dump code /dump.php
@@ -270,3 +270,11 @@ class User {
         </div>
   */
   //'body' => $this->faker->paragraph
+
+  /*
+  <div class="hidden lg:block">
+    <a href="/posts/{{ $post->slug }}"
+        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
+    >Read Mor {!! $post->title !!}</a>
+</div>
+*/
--- blog/resources/views/components/card.blade.php
+++
@@ -1,3 +0,0 @@
-
-<p class="text-blue-900 text-lg" >CARD CARD CARD CARD CARD</p>
-
