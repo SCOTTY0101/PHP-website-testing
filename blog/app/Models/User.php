e93a0edfb13ccef01ebb6772f25795ac6da2eb23
SCOTTY0101 <alan777555@hotmail.com>
2023-06-05 21:01:34 +1000

    Password mutator bcrypt added

--- blog/app/Models/User.php
+++ blog/app/Models/User.php
@@ -45,6 +45,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    //Mutates password/string to crypt before saving in db.
+    public function setPasswordAttribute($password) 
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
+
     public function posts() 
     {
         return $this->hasMany(Post::class);
