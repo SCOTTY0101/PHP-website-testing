26c18192829c06a384f4925f4ead40e527974500
SCOTTY0101 <alan777555@hotmail.com>
2023-07-02 20:14:30 +1000

    default tailwind added

--- blog/app/Providers/AppServiceProvider.php
+++ blog/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+//Brings in default tailwind.
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Paginator::useTailwind();
     }
 }
